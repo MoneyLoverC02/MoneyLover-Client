9348d877c11ac550b0664e118c0ff19b04d2c5c5
HieuT23511 <hieut23511@gmail.com>
2023-08-02 14:46:31 +0700

    dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Register from "./pages/Register";
+import NestedModal from './components/NestedModal';
 
 function App() {
-  return (
-
-  );
+    return (
+        <Routes>
+            <Route path={"/login"} element={<Login/>}/>
+            <Route path={"/register"} element={<Register/>}/>
+            <Route path={"/create"} element={<NestedModal/>}></Route>
+        </Routes>
+    );
 }
 
 export default App;
