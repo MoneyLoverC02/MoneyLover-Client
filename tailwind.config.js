0d373e01762b29e7d36e54e2b5e8d9f510965874
HieuT23511 <hieut23511@gmail.com>
2023-08-03 17:38:56 +0700

    api login-register

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        darkgreen: '#00710f',
+        lightgreen: '#2db84c',
+        normalgreen:'#00bc2a',
+        lightblue: '#3F51B5',
+      }
+    },
+    plugins: [],
+  }
 }
