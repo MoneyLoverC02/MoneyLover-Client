016b88987f8d76ae7caae415145eb3267f7da857
HieuT23511 <hieut23511@gmail.com>
2023-08-07 14:45:03 +0700

    login w gg

--- src/pages/LoginOrRegister.js
+++ src/pages/LoginOrRegister.js
@@ -7,6 +7,7 @@ import { useDispatch } from "react-redux";
 import { loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "../redux/authSlice";
 import { WalletService } from "../services/wallet.service";
 import { getAllWallet, setWalletSelect } from "../redux/walletSlice";
+import LoginWithGoogle from "./LoginWithGg";
 
 
 const validateInput = Yup.object({
@@ -121,17 +122,9 @@ export default function LoginOrRegister({ props }) {
                                     <span className="text-slate-500">Using social networking accounts</span>
                                 </div>
                                 <div className="flex flex-col gap-[18px] min-w-[275px]">
-                                    <button type="button" className="pl-4 border-2 group border-rose-400 rounded-lg py-2 text-rose-400 font-bold hover:bg-rose-400 hover:text-white text-left">
-                                        <div className="flex items-center">
-                                            <svg className="mr-4 group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
-                                                <path fill="#fbc02d" className="group-hover:fill-white" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
-                                                <path fill="#e53935" className="group-hover:fill-white" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
-                                                <path fill="#4caf50" className="group-hover:fill-white" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
-                                                <path fill="#1565c0" className="group-hover:fill-white" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
-                                            </svg>
-                                            Connect with google
+                                        <div>
+                                           <LoginWithGoogle/>
                                         </div>
-                                    </button>
                                     <button type="button" className=" pl-4 border-2 group border-blue-500 rounded-lg py-2 text-blue-500 font-bold hover:bg-blue-500 hover:text-white text-left">
                                         <div className=" flex items-center">
                                             <svg className="mr-4" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
