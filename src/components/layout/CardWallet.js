3e6f8b80db03519be942e87d295b19f978027bfd
Hyboy95 <temilator@gmail.com>
2023-08-08 18:02:17 +0700

    tranfer money

--- src/components/layout/CardWallet.js
+++ src/components/layout/CardWallet.js
@@ -3,7 +3,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {
     AppBar, Box, Card, Checkbox, Container, FormControlLabel, Grid, IconButton, Slide, Stack, Toolbar, Typography
 } from "@mui/material";
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 import ClearIcon from "@mui/icons-material/Clear";
 import ModalDeleteWallets from './ModalDeleteWallets';
@@ -11,8 +11,9 @@ import { WalletService } from '../../services/wallet.service';
 import { setWalletSelect } from '../../redux/walletSlice';
 import UpdateModal from '../modals/UpdateModal';
 import NestedModal from '../modals/NestedModal';
-import {useNavigate} from 'react-router-dom';
-import {useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import TranferModal from '../modals/TranferModal';
 
 export default function CardWallet() {
     const dispatch = useDispatch();
@@ -20,6 +21,7 @@ export default function CardWallet() {
     const [checked, setChecked] = React.useState(false);
     const [openFormCreate, setOpenFormCreate] = React.useState(false);
     const [openFormUpdate, setOpenFormUpdate] = React.useState(false);
+    const [openFormTranfer, setOpenFormTranfer] = React.useState(false);
     const user = useSelector(state => state.auth.login.currentUser);
     const allWallet = useSelector(state => state.wallet.allWallet);
     const walletSelect = useSelector(state => state.wallet.walletSelect);
@@ -62,15 +64,20 @@ export default function CardWallet() {
         setIsChecked(!isChecked)
 
     };
-
     const handleOpenFormTranfer = () => {
-
+        setOpenFormTranfer(true)
+    }
+    const handleCloseFormTranfer = () => {
+        setOpenFormTranfer(false)
+    }
+    const handleSubmitFormTranfer = () => {
+        handleCloseFormTranfer()
     }
 
     return (<div>
         <Slide direction="down" in={true} mountOnEnter unmountOnExit>
             <div>
-                <AppBar sx={{position: 'relative', backgroundColor: "white", color: "black"}}>
+                <AppBar sx={{ position: 'relative', backgroundColor: "white", color: "black" }}>
                     <Toolbar>
                         <IconButton
                             edge="start"
@@ -78,13 +85,13 @@ export default function CardWallet() {
                             onClick={handleClose}
                             aria-label="close"
                         >
-                            <ArrowBackIcon/>
+                            <ArrowBackIcon />
                         </IconButton>
-                        <Typography sx={{ml: 2, flex: 1}} variant="h6" component="div">
+                        <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
                             My Wallets
                         </Typography>
-                        <Button onClick={handleOpenFormCreate} variant="contained" sx={{backgroundColor: "#1aa333"}}
-                                disableElevation>
+                        <Button onClick={handleOpenFormCreate} variant="contained" sx={{ backgroundColor: "#1aa333" }}
+                            disableElevation>
                             <b>ADD WALLET</b>
                         </Button>
                     </Toolbar>
@@ -93,28 +100,28 @@ export default function CardWallet() {
         </Slide>
         <Slide direction="down" in={true} mountOnEnter unmountOnExit>
             <Container>
-                <Box sx={{margin: " 50px auto"}}>
+                <Box sx={{ margin: " 50px auto" }}>
 
                     <Grid container spacing={2}>
                         <Grid item xs={4}>
-                            <Card sx={{maxWidth: 578}} variant="outlined">
+                            <Card sx={{ maxWidth: 578 }} variant="outlined">
                                 <Box sx={{
                                     position: 'relative', backgroundColor: "#f4f4f4", color: "black", height: "40px",
                                 }}>
-                                    <p style={{padding: "5px 10px"}}>Ví của tôi</p>
+                                    <p style={{ padding: "5px 10px" }}>Ví của tôi</p>
                                 </Box>
                                 <>
                                     {allWallet.length > 0 && allWallet.map(wallet => (
                                         <Button onClick={() => handleOpenSlide(wallet.id)} variant="outlined"
-                                                fullWidth="575px" color="success"
-                                                sx={{color: "black", justifyContent: "left", textAlign: "left"}}>
+                                            fullWidth="575px" color="success"
+                                            sx={{ color: "black", justifyContent: "left", textAlign: "left" }}>
                                             <div>
                                                 <img src={wallet.icon.icon}
-                                                     style={{
-                                                         width: "40px", height: "40px", margin: "15px", float: "left"
-                                                     }} alt=""/>
-                                                <div style={{float: "left", margin: "15px"}}>
-                                                    <span className='lowercase'>{wallet.name}</span><br/>
+                                                    style={{
+                                                        width: "40px", height: "40px", margin: "15px", float: "left"
+                                                    }} alt="" />
+                                                <div style={{ float: "left", margin: "15px" }}>
+                                                    <span className='lowercase'>{wallet.name}</span><br />
                                                     <span className='lowercase'>{wallet.currency.sign} </span>
                                                     <span>{wallet.amountOfMoney} </span>
                                                 </div>
@@ -132,71 +139,73 @@ export default function CardWallet() {
                                         height: "50px",
                                         borderBottom: "1px solid #ececec"
                                     }}>
-                                        <div style={{padding: "5px 10px",}}><Button sx={{color: "black"}}
-                                                                                    onClick={handleCloseSlide}><ClearIcon
-                                            sx={{float: "left"}}/></Button>
-                                            <b style={{marginLeft: "30px"}}>Wallet details</b>
-                                            <Stack direction="row" sx={{float: "right"}} spacing={2}>
-                                                <ModalDeleteWallets sx={{height: "402px"}}
-                                                                    idWallet={walletSelect.id}
-                                                                    onClose={handleCloseSlide}/>
+                                        <div style={{ padding: "5px 10px", }}><Button sx={{ color: "black" }}
+                                            onClick={handleCloseSlide}><ClearIcon
+                                                sx={{ float: "left" }} /></Button>
+                                            <b style={{ marginLeft: "30px" }}>Wallet details</b>
+                                            <Stack direction="row" sx={{ float: "right" }} spacing={2}>
+                                                <ModalDeleteWallets sx={{ height: "402px" }}
+                                                    idWallet={walletSelect.id}
+                                                    onClose={handleCloseSlide} />
                                                 <Button onClick={handleOpenFormUpdate} color='success'>EDIT</Button>
                                             </Stack>
                                         </div>
                                     </Box>
                                     <div fullWidth color="success"
-                                         sx={{color: "black", justifyContent: "left", textAlign: "left"}}>
+                                        sx={{ color: "black", justifyContent: "left", textAlign: "left" }}>
                                         <div>
                                             <img src={walletSelect.icon.icon}
-                                                 style={{
-                                                     width: "40px", height: "40px", margin: "15px", float: "left"
-                                                 }} alt=""/>
-                                            <div style={{textAlign: "left", margin: "15px"}}>
-                                                <span className='lowercase'>{walletSelect.name}</span><br/>
+                                                style={{
+                                                    width: "40px", height: "40px", margin: "15px", float: "left"
+                                                }} alt="" />
+                                            <div style={{ textAlign: "left", margin: "15px" }}>
+                                                <span className='lowercase'>{walletSelect.name}</span><br />
                                                 <span className='lowercase'>{walletSelect.currency.sign} </span>
                                                 <span>{walletSelect.amountOfMoney} </span>
                                             </div>
                                         </div>
                                     </div>
-                                    <div style={{borderTop: "1px solid #ececec"}}>
+                                    <div style={{ borderTop: "1px solid #ececec" }}>
                                         <Container>
                                             <Grid icon xs={4}>
                                                 <FormControlLabel
-                                                    control={<Checkbox style={{color: '#1aa333'}}
-                                                                       checked={isChecked}
-                                                                       onChange={handleCheckboxChange}/>}
-                                                    label="lưu trữ"/>
+                                                    control={<Checkbox style={{ color: '#1aa333' }}
+                                                        checked={isChecked}
+                                                        onChange={handleCheckboxChange} />}
+                                                    label="lưu trữ" />
                                             </Grid>
                                         </Container>
                                     </div>
                                     {isChecked === false ? (
                                         <>
-                                            <Button onClick={handleOpenFormTranfer} fullWidth sx={{ borderTop: "1px solid #ececec" , color:"green" }}>
+                                            <Button onClick={handleOpenFormTranfer} fullWidth sx={{ borderTop: "1px solid #ececec", color: "green" }}>
                                                 <Grid item xs={12}>
-                                                    <b>TRANFERMONEY</b>
+                                                    <b>TRANFER MONEY</b>
                                                 </Grid>
                                             </Button>
-                                            <Button fullWidth sx={{ borderTop: "1px solid #ececec" , color:"green" }}>
+                                            <Button fullWidth sx={{ borderTop: "1px solid #ececec", color: "green" }}>
                                                 <Grid item xs={12}>
                                                     <b>SHARE VÍ</b>
                                                 </Grid>
                                             </Button>
                                         </>
                                     ) : (
-                                        <Button fullWidth sx={{ borderTop: "1px solid #ececec" , color:"green" }}>
+                                        <Button fullWidth sx={{ borderTop: "1px solid #ececec", color: "green" }}>
                                             <Grid item xs={12}>
                                                 <b>SHARE VÍ</b>
                                             </Grid>
                                         </Button>
                                     )}
-                            </Card>
-                        </Grid>
-                            </Slide>}
+                                </Card>
+                            </Grid>
+                        </Slide>}
                     </Grid>
                     <UpdateModal isOpen={openFormUpdate} onClose={handleCloseFormUpdate}
-                                 onSubmit={handleSubmitFormUpdate}/>
+                        onSubmit={handleSubmitFormUpdate} />
                     <NestedModal isOpen={openFormCreate} onClose={handleCloseFormCreate}
-                                 onSubmit={handleSubmitFormCreate}/>
+                        onSubmit={handleSubmitFormCreate} />
+                    <TranferModal isOpen={openFormTranfer} onClose={handleCloseFormTranfer}
+                        onSubmit={handleSubmitFormTranfer} />
                 </Box>
             </Container>
         </Slide>
--- src/components/modals/TranferModal.js
+++ src/components/modals/TranferModal.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import { Box, Modal } from '@mui/material';
 import CurrencyModal from './CurrencyModal';
-import IconModal from './IconModal';
 import { useDispatch, useSelector } from 'react-redux';
 import { WalletService } from '../../services/wallet.service';
 import { getAllWallet, setWalletSelect } from '../../redux/walletSlice';
-import FreeSoloCreateOption from '../inputs/AutoCompateInput';
+import WalletSelectModal from './WalletSelectModal';
 
 const style = {
     position: 'absolute',
@@ -20,65 +19,50 @@ const style = {
 
 export default function TranferModal({ isOpen, onClose, onSubmit }) {
     const [isValid, setIsValid] = React.useState(true);
+    const [walletReceived, setWalletReceived] = React.useState();
     const walletSelect = useSelector(state => state.wallet.walletSelect);
-    const allWallet = useSelector(state => state.wallet.allWallet);
-    const [currencySelect, setCurrencySelect] = React.useState(walletSelect?.currency);
-    const [iconSelect, setIconSelect] = React.useState(walletSelect?.icon);
+    // const allWallet = useSelector(state => state.wallet.allWallet);
     const user = useSelector(state => state.auth.login.currentUser);
-    const [dataInput, setDataInput] = React.useState({ name: walletSelect?.name, amountOfMoney: walletSelect?.amountOfMoney });
+    const [moneyInput, setMoneyInput] = React.useState(0);
+    const [checkMoney, setCheckMoney] = React.useState(true);
     const dispatch = useDispatch();
-    const [checkName, setCheckName] = React.useState(true);
 
-    React.useEffect(() => {
-        setIconSelect(walletSelect?.icon);
-        setCurrencySelect(walletSelect?.currency);
-        setDataInput({ name: walletSelect?.name, amountOfMoney: walletSelect?.amountOfMoney })
-    }, [walletSelect]);
     React.useEffect(() => {
         let token = localStorage.getItem('token')
         WalletService.getAllWallet(user.id, token).then(res => {
             dispatch(getAllWallet(res.data.walletList));
         })
     }, [])
-    const handleSelectIcon = (icon) => {
-        setIconSelect(icon);
+
+    const handleSelectWallet = (wallet) => {
+        setWalletReceived(wallet);
     }
-    const handleSelectCurrency = (currency) => {
-        setCurrencySelect(currency);
-    }
-    const handleFocus = () => {
-        document.getElementById("note").focus();
-    };
+
     const handleChange = (e) => {
-        let name = '';
-        let data = { ...dataInput, [e.target.name]: e.target.value };
-        setDataInput(data);
-        if (e.target.name === 'name') {
-            name = e.target.value;
-            let walletListCheck = allWallet.filter(item => item.name !== walletSelect.name);
-            let wallet = walletListCheck.find(item => item.name === name);
-            wallet ? setCheckName(false) : setCheckName(true);
-        }
+        let data = e.target.value;
+        data > walletSelect.amountOfMoney ? setCheckMoney(false):setCheckMoney(true);
+        setMoneyInput(data);
         handleCheckValid(e);
     }
     const handleCheckValid = (e) => {
-        let data = { ...dataInput, [e.target.name]: e.target.value };
-        if (data.name && data.amountOfMoney > 0) setIsValid(true)
+        let money = e.target.value;
+        if (money > 0 ) setIsValid(true)
         else setIsValid(false);
     }
     const handleSubmit = () => {
-        let name = dataInput.name;
-        let iconID = iconSelect?.id;
-        let currencyID = currencySelect?.id;
-        let amountOfMoney = dataInput.amountOfMoney;
-        let token = localStorage.getItem('token')
-        WalletService.updateWallet(user.id, walletSelect.id, { name, iconID, currencyID, amountOfMoney }).then((res) => {
-            let updateWallet = res.data.updateWallet;
-            WalletService.getAllWallet(user.id, token).then(res => {
-                dispatch(setWalletSelect(updateWallet));
-                dispatch(getAllWallet(res.data.walletList));
-                onSubmit();
-            })
+        let walletIDReceived = walletReceived.id;
+        let money = +moneyInput;
+        let token = localStorage.getItem('token');
+        WalletService.tranferMoney(user.id, walletSelect.id, { walletIDReceived, money }).then((res) => {
+            if (res.data.message === 'Money transfer success!') {
+                setMoneyInput(0);
+                let walletTranfer = res.data.walletTransfer;
+                WalletService.getAllWallet(user.id, token).then(res => {
+                    dispatch(setWalletSelect(walletTranfer));
+                    dispatch(getAllWallet(res.data.walletList));
+                    onSubmit();
+                })
+            }
         }).catch(err => console.log(err.message));
     }
 
@@ -93,37 +77,28 @@ export default function TranferModal({ isOpen, onClose, onSubmit }) {
                     <div className='px-6 py-5 border-b-[1px] border-gray-300'>
                         <p className='text-xl font-semibold'>Transfer money to another wallet</p>
                     </div>
-                    <FreeSoloCreateOption/>
-                    {/* <div className='p-6'>
-                        <div className='flex item-center justify-center'>
-                            <div onClick={handleFocus} className='mb-4 py-[5px] px-[15px] border w-full border-gray-300 rounded-lg hover:border-gray-500 hover: cursor-pointer'>
-                                <p className='text-[12px] pb-[3px] text-slate-400'>Wallet name</p>
-                                <div className='pb-1'>
-                                    <input onChange={handleChange} className='inputAdd w-full h-[27px] text-[17px] focus:outline-none' tabIndex="-1" type="text" name="name" value={dataInput.name} placeholder="Your wallet name?" id="note" />
-                                </div>
-                            </div>
-                        </div>
+                    <div className='p-6'>
                         <div className='flex items-center justify-center mb-6'>
                             <div className='w-64 mr-4 py-1 pl-4 pr-3 border border-gray-300 rounded-lg hover:border-gray-500 hover:cursor-pointer'>
-                                <CurrencyModal selectCurrency={handleSelectCurrency} currencyBeforeUpdate={walletSelect?.currency} />
+                                <WalletSelectModal walletDesSelect={handleSelectWallet} />
                             </div>
                             <div className='w-44 py-[7.25px] pl-4 pr-3 border border-gray-300 rounded-lg hover:border-gray-500'>
-                                <p className='text-[12px] pb-[3px] text-slate-400'>Initial Balance</p>
+                                <p className='text-[12px] pb-[3px] text-slate-400'>Amount Of Money</p>
                                 <div className='pb-1'>
-                                    <input onChange={handleChange} className='inputAdd w-full h-[26px] text-[17px] focus:outline-none' tabIndex="-1" type="number" placeholder='0' name="amountOfMoney" value={dataInput.amountOfMoney} required />
+                                    <input onChange={handleChange} className='inputAdd w-full h-[26px] text-[17px] focus:outline-none' tabIndex="-1" type="number" placeholder='0' name="money" value={moneyInput} required />
                                 </div>
                             </div>
                         </div>
-                        <div className=' text-center'>{!checkName ? (<p className="text-red-500 text-sm mt-3">Tên ví đã trùng!</p>) : null}</div>
+                        <div className=' text-center'>{!checkMoney ? (<p className="text-red-500 text-sm mt-3">Số tiền chuyển phải nhỏ hơn số dư!</p>) : null}</div>
                         <div className='pt-[13px] pb-5 flex text-center'>
                             <input className='w-4 h-4 hover: cursor-pointer mt-1' type="checkbox" name="vehicle1" value="Bike" required />
                             <div className='ml-3'>
                                 <p>Chấp nhận điều khoản</p>
                             </div>
                         </div>
-                    </div> */}
+                    </div>
                     <div className='py-[14px] px-6 flex justify-end'>
-                        <button type='button' onClick={handleSubmit} className='bg-lightgreen text-white text-sm font-medium py-2 px-8 uppercase rounded disabled:bg-slate-400' disabled={!isValid || !checkName}>Save</button>
+                        <button type='button' onClick={handleSubmit} className='bg-lightgreen text-white text-sm font-medium py-2 px-8 uppercase rounded disabled:bg-slate-400' disabled={!isValid || !checkMoney}>Save</button>
                     </div>
                 </Box>
             </Modal>
