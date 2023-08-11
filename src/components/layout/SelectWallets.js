941fb231391ec1121a009a324e24165db09615f4
ducanh2710203 <duc2710203@gmail.com>
2023-08-11 11:34:00 +0700

    pull dev

--- src/components/layout/SelectWallets.js
+++ src/components/layout/SelectWallets.js
@@ -1,87 +1,3 @@
-// import * as React from 'react';
-// import PropTypes from 'prop-types';
-// import Button from '@mui/material/Button';
-// import Avatar from '@mui/material/Avatar';
-// import List from '@mui/material/List';
-// import ListItem from '@mui/material/ListItem';
-// import ListItemAvatar from '@mui/material/ListItemAvatar';
-// import ListItemButton from '@mui/material/ListItemButton';
-// import ListItemText from '@mui/material/ListItemText';
-// import DialogTitle from '@mui/material/DialogTitle';
-// import Dialog from '@mui/material/Dialog';
-// import PersonIcon from '@mui/icons-material/Person';
-// import Typography from '@mui/material/Typography';
-// import {blue} from '@mui/material/colors';
-// import {useSelector} from 'react-redux';
-// function SimpleDialog(props) {
-//     const { onClose, selectedValue, open } = props;
-//     const walletList = useSelector(state => state.wallet.allWallet);
-
-//     const handleClose = () => {
-//         onClose(selectedValue);
-//     };
-
-//     const handleListItemClick = (value) => {
-//         onClose(value);
-//     };
-
-//     return (<Dialog onClose={handleClose}  open={open}>
-//         <DialogTitle>Excluded from Total</DialogTitle>
-//         <List sx={{pt: 0}}>
-//             {walletList.length > 0 && walletList.map((wallet) => (<ListItem disableGutters>
-//                 <ListItemButton onClick={() => handleListItemClick(wallet)} key={wallet.id}>
-//                     <ListItemAvatar>
-//                         <Avatar sx={{bgcolor: blue[100], color: blue[600]}}>
-//                             <PersonIcon/>
-//                         </Avatar>
-//                     </ListItemAvatar>
-//                     <ListItemText primary={wallet.name}/>
-//                 </ListItemButton>
-//             </ListItem>))}
-//         </List>
-//     </Dialog>);
-// }
-
-// SimpleDialog.propTypes = {
-//     onClose: PropTypes.func.isRequired, open: PropTypes.bool.isRequired, selectedValue: PropTypes.string.isRequired,
-// };
-
-// export default function SelectWallets() {
-//     const [open, setOpen] = React.useState(false);
-//     const allWallet = useSelector(state => state.wallet.allWallet)
-//     const [selectedName, setSelectedName] = React.useState( allWallet?.length> 0 ? allWallet[0].name : '')
-//     const [selectedMoney, setSelectedMoney] = React.useState( allWallet?.length> 0 ? allWallet[0].amountOfMoney : 0);
-
-//     const handleClickOpen = () => {
-//         setOpen(true);
-//     };
-
-//     const handleClose = (value) => {
-//         setOpen(false);
-//         console.log(value);
-//         if (typeof value !== 'function' && value !== null) {
-//             setSelectedName(value.name);
-//             setSelectedMoney(value.amountOfMoney);
-//         }
-//     };
-
-//     return (<div>
-
-//         <Button sx={{color: "black", justifyContent: "left", textTransform: 'lowercase'}} onClick={handleClickOpen}>
-//             {selectedName}
-//         </Button>
-//         <Typography variant="subtitle1" component="div">
-//             + {selectedMoney} đ
-//         </Typography>
-//         <SimpleDialog
-//             selectedValue={setSelectedName}
-//             open={open}
-//             onClose={handleClose}
-//         />
-//     </div>);
-// }
-
-
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@mui/material/Button';
@@ -101,7 +17,7 @@ import { setWalletSelect } from '../../redux/walletSlice';
 function SimpleDialog(props) {
     const { onClose, selectedValue, open } = props;
     const walletList = useSelector(state => state.wallet.allWallet);
-    // const walletSelect = useSelector(state => state.wallet.walletSelect);
+    const walletSelect = useSelector(state => state.wallet.walletSelect);
 
     const handleClose = () => {
 
@@ -116,15 +32,25 @@ function SimpleDialog(props) {
 
     return (<Dialog onClose={handleClose} open={open}>
         <DialogTitle>Excluded from Total</DialogTitle>
-        <List sx={{ pt: 0 }}>
+        <List sx={{ pt: 0, width:"500px" }}>
             {walletList?.length > 0 && walletList.map((wallet) => (<ListItem disableGutters>
                 <ListItemButton onClick={() => handleListItemClick(wallet)} key={wallet.id}>
                     <ListItemAvatar>
-                        <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                            <PersonIcon />
+                        <Avatar sx={{ bgcolor: blue[100], color: blue[600] } }>
+                            <img src={wallet.icon.icon} alt=""/>
                         </Avatar>
                     </ListItemAvatar>
-                    <ListItemText primary={wallet.name} />
+                    {/*<ListItemText primary={wallet.name +" : "+wallet.amountOfMoney +" "+ wallet.currency.sign } />*/}
+                    <ListItemText
+                        primary={wallet.name}
+                        secondary={
+                            <React.Fragment>
+                                <Typography variant="body2" color="text.secondary">
+                                    {wallet.amountOfMoney} {wallet.currency.sign}
+                                </Typography>
+                            </React.Fragment>
+                        }
+                    />
                 </ListItemButton>
             </ListItem>))}
         </List>
@@ -145,12 +71,12 @@ export default function SelectWallets() {
     // const [selectedMoney, setSelectedMoney] = React.useState( allWallet.length> 0 ? allWallet[0].amountOfMoney : 0);
 
     const handleClickOpen = () => {
+        console.log(walletSelect)
         setOpen(true);
     };
 
     const handleClose = (value) => {
         setOpen(false);
-        console.log(typeof value)
         if (typeof value === "object" && value !== null) {
             dispatch(setWalletSelect(value))
         }
@@ -162,7 +88,7 @@ export default function SelectWallets() {
             {walletSelect?.name}
         </Button>
         <Typography variant="subtitle1" component="div">
-            + {walletSelect?.amountOfMoney} đ
+            {walletSelect.amountOfMoney>0?"+":null} {walletSelect?.amountOfMoney} {walletSelect.currency.sign}
         </Typography>
         <SimpleDialog
             selectedValue={walletSelect?.name}
